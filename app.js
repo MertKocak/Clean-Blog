b51160f2255fb97f38147eab1a164864157fa064
Mert Koçak <mertkocak.2811@gmail.com>
2024-10-18 16:00:51 +0300

    mongodb

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+const Post = require("./models/Post");
+const path = require("path");
+
 const app = express();
+
+//connect db
+mongoose.connect("mongodb://localhost/cleanblog-test-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+//template engine
+app.set("view engine", "ejs");
 
 // middleware
 app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
-app.set("view engine", "ejs");
-
-app.get("/", (req, res) => {
-  res.render('index');
+app.get("/", async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', {posts});
 });
 app.get("/about", (req, res) => {
   res.render('about');
@@ -18,6 +32,10 @@ app.get("/post", (req, res) => {
 });
 app.get("/add_post", (req, res) => {
   res.render('add_post');
+});
+app.post("/posts", async (req, res) => {
+  await Post.create(req.body);
+  res.redirect("/");
 });
 
 const port = 3000;
