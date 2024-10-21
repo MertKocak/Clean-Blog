6e8dc0061df1832daa962680b04f160c224bb712
Mert Koçak <mertkocak.2811@gmail.com>
2024-10-21 21:06:35 +0300

    Delete ve Update butonları eklendi ve MVC düzenlemesi yapıldı.

--- app.js
+++ app.js
@@ -1,46 +1,36 @@
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const Post = require("./models/Post");
-const path = require("path");
+const methodOverride = require("method-override");
+const postController = require("./controllers/postControllers");
+const pageController = require("./controllers/pageControllers");
 
 const app = express();
 
 //connect db
 mongoose.connect("mongodb://localhost/cleanblog-test-db", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+  useUnifiedTopology: true,
+});
 
 //template engine
 app.set("view engine", "ejs");
 
 // middleware
 app.use(express.static("public"));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method", { methods: ["POST", "GET"] }));
 
-app.get("/", async (req, res) => {
-  const posts = await Post.find({});
-  res.render('index', {posts});
-});
-app.get("/posts/:id", async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.render('post', {post});
-});
-app.get("/about", (req, res) => {
-  res.render('about');
-});
-app.get("/post", (req, res) => {
-  res.render('post');
-});
-app.get("/add_post", (req, res) => {
-  res.render('add_post');
-});
-app.post("/posts", async (req, res) => {
-  await Post.create(req.body);
-  res.redirect("/");
-});
+app.get("/", postController.getAllPosts);
+app.get("/posts/:id", postController.getPost);
+app.delete("/posts/:id", postController.deletePost);
+app.put("/posts/:id", postController.updatePost);
+app.post("/posts", postController.createPost);
+
+app.get("/posts/edit/:id", pageController.getEditPage);
+app.get("/about", pageController.getAboutPage);
+app.get("/add_post", pageController.getAddPostPage);
 
 const port = 3000;
 
